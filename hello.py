1a25c9c99591a93e86872146ade95a3985043d11
peefer66 <peefer1966@gmail.com>
2018-06-17 22:14:55 +0100

    lec 67

--- hello.py
+++ hello.py
@@ -10,9 +10,14 @@ def index():
 def info():
     return '<h1> The world is round </h1>'
 
-@app.route('/country/<name>')
-def country(name):
-    return '<h1> This page is for {} </h1>'.format(name.upper())
+@app.route('/puppy/<name>')
+def puppy(name):
+    return '<h1> This page is for {} </h1>'.format(name.upper()) # format to uppercase
+
+@app.route('/puppy_latin/<name>')
+def puppy_latin(name):
+    name_len = len(name)
+    return 'Name length = {}'.format(name_len)
 
 
 if __name__ == '__main__':
